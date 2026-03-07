135e97de298ffa85f80d0106e42665a63f6452fc
Braden Ganetsky <braden.ganetsky@gmail.com>
2026-03-07 01:05:50 -0600

    gdb: Add pretty-printer test framework

---
+++ gdb/include/k3/gdb/framework.hpp
@@ -0,0 +1,130 @@
+// Copyright 2026 Braden Ganetsky
+// Distributed under the Boost Software License, Version 1.0.
+// https://www.boost.org/LICENSE_1_0.txt
+
+#ifndef K3_GDB_FRAMEWORK_HPP
+#define K3_GDB_FRAMEWORK_HPP
+
+#include <vector>
+
+#if defined(__clang__)
+#define K3_GDB_NO_INLINE __attribute__((noinline, optnone))
+#elif defined(__GNUC__)
+#define K3_GDB_NO_INLINE __attribute__((noinline, optimize("O0"), noclone))
+#else
+#error "Unsupported compiler"
+#endif
+
+namespace k3::gdb {
+
+template <class... Ts>
+K3_GDB_NO_INLINE void _breakpoint_do_not_call_this_directly(const Ts&...) {}
+
+// template <class T = void>
+K3_GDB_NO_INLINE void test_start(const char* name) {
+    _breakpoint_do_not_call_this_directly(name);
+}
+
+// template <class T = void>
+K3_GDB_NO_INLINE void test_finish(const char* name) {
+    _breakpoint_do_not_call_this_directly(name);
+}
+
+template <class T>
+K3_GDB_NO_INLINE void assert_prints(const T& actual, const char* expected) {
+    _breakpoint_do_not_call_this_directly(actual, expected);
+}
+
+template <class T>
+K3_GDB_NO_INLINE void expect_prints(const T& actual, const char* expected) {
+    _breakpoint_do_not_call_this_directly(actual, expected);
+}
+
+using size_t = decltype(sizeof(0));
+
+// Use UDL to guarantee usage of a string literal
+constexpr size_t operator ""_k3_gdb_hash(const char* s, size_t size) noexcept {
+    size_t hash = 0;
+    for (size_t i = 0; i != size; ++i) {
+        // rotl by 1, then add the new char
+        hash = ((hash << 1) | (hash >> 63)) + static_cast<size_t>(s[i]);
+    }
+    return hash;
+}
+
+class test {
+public:
+    test(const char* name, void(*func)())
+        : _name(name), _func(func)
+    {}
+    void run() const {
+        _run_guard guard{_name};
+        _func();
+    }
+private:
+    const char* _name;
+    void(*_func)();
+    class [[nodiscard]] _run_guard {
+        const char* _name;
+    public:
+        _run_guard(const char* name) {
+            _name = name;
+            test_start(_name);
+        }
+        ~_run_guard() {
+            test_finish(_name);
+        }
+    };
+};
+
+class runner {
+public:
+    static runner& get() {
+        static runner r;
+        return r;
+    }
+    void exec(int argc, const char* const argv[]) {
+        for (const auto& t : _tests) {
+            t.run();
+        }
+    }
+    bool add(test t) {
+        _tests.push_back(std::move(t));
+        return true;
+    }
+private:
+    runner() = default;
+    std::vector<test> _tests;
+};
+
+inline void main(const int argc, const char* const* const argv) {
+    runner::get().exec(argc, argv);
+}
+
+} // namespace k3::gdb
+
+#define K3_GDB_CAT_(A, B) A ## B
+#define K3_GDB_CAT(A, B) K3_GDB_CAT_(A, B)
+
+#define TEST(NAME)                                                          \
+    template <size_t hash>                                                  \
+    class k3_gdb_test_impl_;                                                \
+    template <>                                                             \
+    class k3_gdb_test_impl_<(K3_GDB_CAT(NAME, _k3_gdb_hash))> {             \
+    private:                                                                \
+        static void _k3_gdb_run();                                          \
+        static inline const bool _k3_gdb_init = ::k3::gdb::runner::get()    \
+            .add(::k3::gdb::test((NAME), &_k3_gdb_run));                    \
+    };                                                                      \
+    void k3_gdb_test_impl_<(K3_GDB_CAT(NAME, _k3_gdb_hash))>::_k3_gdb_run()
+
+namespace k3::gdb::checks {
+
+using k3::gdb::assert_prints;
+using k3::gdb::expect_prints;
+
+} // namespace k3::gdb::checks
+
+using k3::gdb::operator ""_k3_gdb_hash;
+
+#endif // K3_GDB_FRAMEWORK_HPP
---
+++ gdb/include/k3/gdb/framework.py
@@ -0,0 +1,158 @@
+# Copyright 2026 Braden Ganetsky
+# Distributed under the Boost Software License, Version 1.0.
+# https://www.boost.org/LICENSE_1_0.txt
+
+import gdb
+
+import enum
+import json
+import re
+import sys
+
+breakpoint = gdb.Breakpoint("k3::gdb::_breakpoint_do_not_call_this_directly")
+breakpoint.commands = "k3_gdb\ncontinue"
+breakpoint.silent = True
+
+class Fatality(enum.Enum):
+    NON_FATAL = 0
+    FATAL = 1
+
+class Error:
+    def __init__(self,
+                 fatality: Fatality,
+                 file: str,
+                 line: int,
+                 expected: str,
+                 actual: str,
+                 ):
+        self.fatality: Fatality = fatality
+        self.file : str = file
+        self.line : int = line
+        self.expected : str = expected
+        self.actual : str = actual
+
+class TestResult:
+    def __init__(self, name: str):
+        self.name : str = name
+        self.num_checks : int = 0
+        self.errors : list[Error] = []
+
+class TestRunner:
+    def __init__(self):
+        self.framework_errors : list[str] = []
+        self.test_results : dict[str, TestResult] = {}
+        self.current_test_results : TestResult | None = None
+        self.skip_to_next_test : bool = False
+runner = TestRunner()
+
+def read_char_ptr(frame: gdb.Frame, identifier: str) -> str:
+    value = frame.read_var(identifier)
+    assert f"{value.type}" == "const char *"
+    match = re.search(r"0x[A-Fa-f0-9]+ (\".*\")$", f"{value}")
+    assert match is not None
+    return json.loads(match.group(1))
+
+class Command(gdb.Command):
+    def __init__(self):
+        super().__init__("k3_gdb", gdb.COMMAND_DATA)
+
+    def invoke(self, arg, from_tty):
+        try:
+            breakpoint_frame = gdb.newest_frame()
+            frame = breakpoint_frame.older()
+            frame.select()
+
+            call_info = frame.older().find_sal()
+
+            if frame.name() == "k3::gdb::test_start":
+                test_name = read_char_ptr(frame, "name")
+                if test_name in runner.test_results:
+                    runner.framework_errors.append(f"Test name seen twice: {test_name}")
+                runner.test_results[test_name] = TestResult(test_name)
+                assert runner.current_test_results is None
+                runner.current_test_results = runner.test_results[test_name]
+
+            elif frame.name() == "k3::gdb::test_finish":
+                test_name = read_char_ptr(frame, "name")
+                if test_name not in runner.test_results:
+                    runner.framework_errors.append(f"Test name missing: {test_name}")
+                assert runner.current_test_results is not None
+                runner.current_test_results = None
+                runner.skip_to_next_test = False
+
+            elif frame.name().startswith("k3::gdb::expect_prints<") or frame.name().startswith("k3::gdb::assert_prints<"):
+                if runner.skip_to_next_test:
+                    return
+                runner.current_test_results.num_checks += 1
+
+                actual = frame.read_var("actual")
+                assert actual.type.code == gdb.TYPE_CODE_REF
+                actual_str = f"{actual.referenced_value()}"
+
+                expected_str = read_char_ptr(frame, "expected")
+
+                if actual_str != expected_str:
+                    fatality = [Fatality.NON_FATAL, Fatality.FATAL][frame.name().startswith("k3::gdb::assert_")]
+                    runner.current_test_results.errors.append(Error(fatality=fatality,
+                                                                    file=call_info.symtab.filename,
+                                                                    line=call_info.line,
+                                                                    expected=expected_str,
+                                                                    actual=actual_str))
+                    if fatality == Fatality.FATAL:
+                        runner.skip_to_next_test = True
+
+            else:
+                call_site = f"{call_info.symtab.filename}:{call_info.line}"
+                runner.framework_errors.append(f"Hit breakpoint in unrecognized stack frame\n\t{frame.name()}\n\t{call_site}")
+
+        except RuntimeError as e:
+            # Quoting the libc++ GDB pretty-printer tests:
+            #   > At this point, lots of different things could be wrong, so don't try to
+            #     recover or figure it out. Don't exit either, because then it's
+            #     impossible debug the framework itself.
+            runner.framework_errors.append(f"Unknown error: {e}")
+
+def exit_handler(event):
+    if len(runner.framework_errors) != 0:
+        print(f"Saw {len(runner.framework_errors)} framework errors:")
+        for e in runner.framework_errors:
+            print(f"    k3::gdb framework error: {e}")
+        sys.exit(1)
+
+    all_passed = True
+
+    for test in runner.test_results.values():
+        if len(test.errors) == 0:
+            print(f"Test {json.dumps(test.name)} passed all {test.num_checks} checks.")
+        else:
+            all_passed = False
+            print(f"Test {json.dumps(test.name)} failed {len(test.errors)} checks of {test.num_checks}.")
+            for e in test.errors:
+                fatality = ["Non-fatal", "Fatal"][e.fatality == Fatality.FATAL]
+                print(f"    {fatality} error at {e.file}:{e.line}\n        Expected: {e.expected}\n        Actual:   {e.actual}")
+        print()
+
+    if (all_passed):
+        sys.exit(0)
+    else:
+        sys.exit(1)
+
+
+
+# The rest of this file is procedurally identical to the
+# libc++ GDB pretty-printer tests, and copies their comments.
+
+# Disable terminal paging
+gdb.execute("set height 0")
+gdb.execute("set python print-stack full")
+
+Command()
+
+# "run" won't return if the program exits; ensure the script regains control.
+gdb.events.exited.connect(exit_handler)
+gdb.execute("run")
+
+# If the program didn't exit, something went wrong, but we don't
+# know what. Fail on exit.
+runner.framework_errors.append("Program did not exit, therefore something went wrong")
+exit_handler(None)
