15b0693d3aaef85dfa7dd46c8664685bb7b7de15
Braden Ganetsky <braden.ganetsky@gmail.com>
2026-03-07 21:00:55 -0600

    gdb: Add test discovery, with the Python variable 'k3_gdb_mode'

--- gdb/include/k3/gdb/framework.py
+++ gdb/include/k3/gdb/framework.py
@@ -39,11 +39,9 @@ class TestResult:
 
 class TestRunner:
     def __init__(self):
-        self.framework_errors : list[str] = []
         self.test_results : dict[str, TestResult] = {}
         self.current_test_results : TestResult | None = None
         self.skip_to_next_test : bool = False
-runner = TestRunner()
 
 def read_char_ptr(frame: gdb.Frame, identifier: str) -> str:
     value = frame.read_var(identifier)
@@ -51,6 +49,12 @@ def read_char_ptr(frame: gdb.Frame, identifier: str) -> str:
     match = re.search(r"0x[A-Fa-f0-9]+ (\".*\")$", f"{value}")
     assert match is not None
     return json.loads(match.group(1))
+
+
+
+framework_errors : list[str] = []
+callbacks_cls : type = None
+runner = TestRunner()
 
 class Command(gdb.Command):
     def __init__(self):
@@ -65,76 +69,150 @@ class Command(gdb.Command):
             call_info = frame.older().find_sal()
 
             if frame.name() == "k3::gdb::test_start":
-                test_name = read_char_ptr(frame, "name")
-                if test_name in runner.test_results:
-                    runner.framework_errors.append(f"Test name seen twice: {test_name}")
-                runner.test_results[test_name] = TestResult(test_name)
-                assert runner.current_test_results is None
-                runner.current_test_results = runner.test_results[test_name]
-
+                callbacks_cls.on_test_start(read_char_ptr(frame, "name"))
             elif frame.name() == "k3::gdb::test_finish":
-                test_name = read_char_ptr(frame, "name")
-                if test_name not in runner.test_results:
-                    runner.framework_errors.append(f"Test name missing: {test_name}")
-                assert runner.current_test_results is not None
-                runner.current_test_results = None
-                runner.skip_to_next_test = False
-
+                callbacks_cls.on_test_finish(read_char_ptr(frame, "name"))
             elif frame.name().startswith("k3::gdb::expect_prints<") or frame.name().startswith("k3::gdb::assert_prints<"):
-                if runner.skip_to_next_test:
-                    return
-                runner.current_test_results.num_checks += 1
-
-                actual = frame.read_var("actual")
-                assert actual.type.code == gdb.TYPE_CODE_REF
-                actual_str = f"{actual.referenced_value()}"
-
-                expected_str = read_char_ptr(frame, "expected")
-
-                if actual_str != expected_str:
-                    fatality = [Fatality.NON_FATAL, Fatality.FATAL][frame.name().startswith("k3::gdb::assert_")]
-                    runner.current_test_results.errors.append(Error(fatality=fatality,
-                                                                    file=call_info.symtab.filename,
-                                                                    line=call_info.line,
-                                                                    expected=expected_str,
-                                                                    actual=actual_str))
-                    if fatality == Fatality.FATAL:
-                        runner.skip_to_next_test = True
-
+                callbacks_cls.on_check_prints(frame, call_info)
             else:
                 call_site = f"{call_info.symtab.filename}:{call_info.line}"
-                runner.framework_errors.append(f"Hit breakpoint in unrecognized stack frame\n\t{frame.name()}\n\t{call_site}")
+                framework_errors.append(f"Hit breakpoint in unrecognized stack frame\n\t{frame.name()}\n\t{call_site}")
 
         except RuntimeError as e:
             # Quoting the libc++ GDB pretty-printer tests:
             #   > At this point, lots of different things could be wrong, so don't try to
             #     recover or figure it out. Don't exit either, because then it's
             #     impossible debug the framework itself.
-            runner.framework_errors.append(f"Unknown error: {e}")
+            framework_errors.append(f"Unknown error: {e}")
 
 def exit_handler(event):
-    if len(runner.framework_errors) != 0:
-        print(f"Saw {len(runner.framework_errors)} framework errors:")
-        for e in runner.framework_errors:
+    if len(framework_errors) != 0:
+        print(f"Saw {len(framework_errors)} k3::gdb framework errors:")
+        for e in framework_errors:
             print(f"    k3::gdb framework error: {e}")
         sys.exit(1)
 
-    all_passed = True
+    callbacks_cls.on_exit_handler() # This function should exit
+    sys.exit(0) # We shouldn't reach this, but just in case
 
-    for test in runner.test_results.values():
-        if len(test.errors) == 0:
-            print(f"Test {json.dumps(test.name)} passed all {test.num_checks} checks.")
+
+
+class RunAllCallbacks:
+    @staticmethod
+    def on_test_start(test_name: str):
+        if test_name in runner.test_results:
+            framework_errors.append(f"Test name seen twice: {test_name}")
+        runner.test_results[test_name] = TestResult(test_name)
+        assert runner.current_test_results is None
+        runner.current_test_results = runner.test_results[test_name]
+
+    @staticmethod
+    def on_test_finish(test_name: str):
+        if test_name not in runner.test_results:
+            framework_errors.append(f"Test name missing: {test_name}")
+        assert runner.current_test_results is not None
+        runner.current_test_results = None
+        runner.skip_to_next_test = False
+
+    @staticmethod
+    def on_check_prints(frame: gdb.Frame, call_info: gdb.Symtab_and_line):
+        if runner.skip_to_next_test:
+            return
+        runner.current_test_results.num_checks += 1
+
+        actual = frame.read_var("actual")
+        assert actual.type.code == gdb.TYPE_CODE_REF
+        actual_str = f"{actual.referenced_value()}"
+
+        expected_str = read_char_ptr(frame, "expected")
+
+        if actual_str != expected_str:
+            fatality = [Fatality.NON_FATAL, Fatality.FATAL][frame.name().startswith("k3::gdb::assert_")]
+            runner.current_test_results.errors.append(Error(fatality=fatality,
+                                                            file=call_info.symtab.filename,
+                                                            line=call_info.line,
+                                                            expected=expected_str,
+                                                            actual=actual_str))
+            if fatality == Fatality.FATAL:
+                runner.skip_to_next_test = True
+
+    @staticmethod
+    def on_exit_handler():
+        all_passed = True
+
+        for test in runner.test_results.values():
+            if len(test.errors) == 0:
+                print(f"Test {json.dumps(test.name)} passed all {test.num_checks} checks.")
+            else:
+                all_passed = False
+                print(f"Test {json.dumps(test.name)} failed {len(test.errors)} checks of {test.num_checks}.")
+                for e in test.errors:
+                    fatality = ["Non-fatal", "Fatal"][e.fatality == Fatality.FATAL]
+                    print(f"    {fatality} error at {e.file}:{e.line}\n        Expected: {e.expected}\n        Actual:   {e.actual}")
+            print()
+
+        if (all_passed):
+            sys.exit(0)
         else:
-            all_passed = False
-            print(f"Test {json.dumps(test.name)} failed {len(test.errors)} checks of {test.num_checks}.")
-            for e in test.errors:
-                fatality = ["Non-fatal", "Fatal"][e.fatality == Fatality.FATAL]
-                print(f"    {fatality} error at {e.file}:{e.line}\n        Expected: {e.expected}\n        Actual:   {e.actual}")
-        print()
+            sys.exit(1)
 
-    if (all_passed):
+
+
+class DiscoveryCallbacks:
+    output_file : str | None = None
+
+    @staticmethod
+    def on_test_start(test_name: str):
+        if test_name in runner.test_results:
+            framework_errors.append(f"Test name seen twice: {test_name}")
+        else:
+            runner.test_results[test_name] = TestResult(test_name)
+        assert runner.current_test_results is None
+        runner.current_test_results = runner.test_results[test_name]
+
+    @staticmethod
+    def on_test_finish(test_name: str):
+        if test_name not in runner.test_results:
+            framework_errors.append(f"Test name missing: {test_name}")
+        assert runner.current_test_results is not None
+        runner.current_test_results = None
+        runner.skip_to_next_test = False
+
+    @staticmethod
+    def on_check_prints(frame: gdb.Frame, call_info: gdb.Symtab_and_line):
+        pass
+
+    @staticmethod
+    def on_exit_handler():
+        output_file = DiscoveryCallbacks.output_file
+        if output_file is None:
+            print(f"Discovered {len(runner.test_results)} k3::gdb tests:")
+            for test in runner.test_results:
+                print(json.dumps(test))
+        else:
+            with open(output_file, "w") as file:
+                for test in runner.test_results:
+                    file.write(f"{json.dumps(test)}\n")
+            print(f"Discovered {len(runner.test_results)} k3::gdb tests, written to {output_file}")
         sys.exit(0)
+
+
+
+callbacks_cls = RunAllCallbacks
+if 'k3_gdb_mode' in globals():
+    k3_gdb_mode = k3_gdb_mode
+    assert type(k3_gdb_mode) == str
+    if k3_gdb_mode == "run_all":
+        pass
+    elif k3_gdb_mode.startswith("discovery"):
+        match = re.search(r"^discovery(?:=(.+))?$", k3_gdb_mode)
+        if match is None:
+            print(f"Unrecognized 'k3_gdb_mode' for 'discovery': {json.dumps(k3_gdb_mode)}")
+            sys.exit(1)
+        callbacks_cls = DiscoveryCallbacks
+        DiscoveryCallbacks.output_file = match.group(1)
     else:
+        print(f"Unrecognized 'k3_gdb_mode': {json.dumps(k3_gdb_mode)}")
         sys.exit(1)
 
 
@@ -154,5 +232,5 @@ gdb.execute("run")
 
 # If the program didn't exit, something went wrong, but we don't
 # know what. Fail on exit.
-runner.framework_errors.append("Program did not exit, therefore something went wrong")
+framework_errors.append("Program did not exit, therefore something went wrong")
 exit_handler(None)
